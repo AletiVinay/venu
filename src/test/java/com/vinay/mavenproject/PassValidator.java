1bc129bccc0e11bc9adacdb0a139ec5d167e8051
vinay <aletiv40@gmail.com>
2022-03-07 15:09:06 +0530

    fristcommit

---
+++ src/test/java/com/vinay/mavenproject/PassValidator.java
@@ -0,0 +1,26 @@
+package com.vinay.mavenproject;
+public class PassValidator {
+
+ public static boolean isValid(String pass)
+
+ {
+
+ if(pass.length()>=5 && pass.length()<=10)
+
+ {
+
+  return true;
+
+ }
+
+ else
+
+ {
+
+  return false;
+
+ }
+
+ }
+
+}
---
+++ src/test/java/com/vinay/mavenproject/PassValidatorTest.java
@@ -0,0 +1,25 @@
+package com.vinay.mavenproject;
+
+
+import static org.junit.Assert.*;
+
+
+
+import org.junit.Test;
+
+public class PassValidatorTest {
+
+ @Test
+
+ public void test() {
+
+ //PassValidator ob=new PassValidator();
+
+ boolean res=PassValidator.isValid("abc123");
+
+ assertEquals(true,res);
+
+ }
+
+}
+
